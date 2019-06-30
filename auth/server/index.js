59a75a030228c2b6e006d7cd0b824e8d7f27ef4d
Eric Schmucker <eschmucker@unstoppablesoftware.com>
2019-06-29 18:29:30 -0600

    Use and configure CORS

--- auth/server/index.js
+++ auth/server/index.js
@@ -5,12 +5,17 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup.
 mongoose.connect('mongodb://localhost:27017/auth', {useNewUrlParser: true});
 
 // App setup.
 app.use(morgan('combined'));
+app.use(cors({
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}));
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
