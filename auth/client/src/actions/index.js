c9aecfaf57550ff10a1b1d054653c176bf89c355
Eric Schmucker <eschmucker@unstoppablesoftware.com>
2019-06-29 18:30:27 -0600

    Implementing signup form

---
+++ auth/client/src/actions/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { AUTH_USER, AUTH_ERROR } from './types';
+
+export const signup = formProps => async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:3090/signup', formProps);
+
+    dispatch({
+      type: AUTH_USER,
+      payload: response.data.token
+    });
+  }
+  catch (exc) {
+    console.log('!!!!');
+    console.log(exc);
+    dispatch({
+      type: AUTH_ERROR,
+      payload: exc
+    });
+  }
+}
--- auth/client/src/components/App.js
+++ auth/client/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
-export default () => {
+export default ({ children }) => {
   return (
     <div>
-      Hi!
+      <Header />
+      {children}
     </div>
   );
 }
